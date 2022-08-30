37e958ebaa8e40e62fa8713fcc1a4414550817d9
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2022-08-30 23:43:40 +0200

    Some file refactorings

--- toc-generation.py
+++
@@ -1,8 +0,0 @@
-# Generate a table of contents for a notebook.
-# Code entirely from StackOverflow: https://stackoverflow.com/questions/21151450/how-can-i-add-a-table-of-contents-to-a-jupyter-jupyterlab-notebook
-# Note: Not entirely from that site, I made a small change to make it immediately display the generated table of contents.
-
-
-
-generate_toc("1 Label classification.ipynb")
-
